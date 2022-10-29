766108aaae32e7f65a83aa2fb88367922692ea4c
Luiz <luizgufamartins@gmail.com>
2022-10-29 10:47:36 -0300

    a

--- src/test/java/com/myproject/project_if66k/controllers/AgendamentoControllerTest.java
+++ src/test/java/com/myproject/project_if66k/controllers/AgendamentoControllerTest.java
@@ -2,6 +2,7 @@ package com.myproject.project_if66k.controllers;
 
 import com.myproject.project_if66k.dao.AgendamentoDAO;
 import com.myproject.project_if66k.models.Agendamento;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,14 +15,10 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
 public class AgendamentoControllerTest {
 
-//    @Mock
-//    AgendamentoDAO agendamentoDAO;
-//
-//    @InjectMocks
-//    AgendamentoController agendamentoController = new AgendamentoController();
+    AgendamentoDAO agendamentoDAO = new AgendamentoDAO("test");
+    
     public AgendamentoControllerTest() {
     }
 
@@ -41,18 +38,35 @@ public class AgendamentoControllerTest {
     public void tearDown() {
     }
 
-//    @Test
-//    public void testReadAll() {
-//        System.out.println("testReadAll");
-//
-//        List<Agendamento> expResult = null;
-//        List<Agendamento> result = new ArrayList<Agendamento>();
-//
-//        when(agendamentoDAO.readAll()).thenReturn(result);
-//        assertNotNull(result);
-//        
-//        // TODO review the generated test code and remove the default call to fail.
-//    }
+    @Test
+    public void testCreate() {
+        System.out.println("Teste de criação de um agendamento");
+        
+        Agendamento age = new Agendamento();
+
+//        age.setId_age(5);
+        age.setId_animal(1);
+        age.setServico_age("Serviço");
+        age.setData_age("10/10/2010 - 10:10");
+        age.setId_func_age(1);
+
+        if (agendamentoDAO.Create(age)) {
+            System.out.println("Teste bem sucedido, salvo com sucesso!");
+        } else {
+            fail("Teste falhou, não foi possível salvar");
+        }
+   
+    }
+    
+    @Test
+    public void testReadAll() {
+        System.out.println("testReadAll");
+
+        List<Agendamento> result = agendamentoDAO.readAll();
+        assertNotNull(result);
+        
+        // TODO review the generated test code and remove the default call to fail. 
+    };
 
     /*@Test
     public void testReadAll() {
@@ -90,26 +104,7 @@ public class AgendamentoControllerTest {
         fail("The test case is a prototype.");
     }
      */
-    @Test
-    public void testCreate() {
-        System.out.println("Teste de criação de um agendamento");
-
-        AgendamentoDAO agendamentoDAO = new AgendamentoDAO("test");
-        Agendamento age = new Agendamento();
-
-//        age.setId_age(5);
-        age.setId_animal(1);
-        age.setServico_age("Serviço");
-        age.setData_age("10/10/2010 - 10:10");
-        age.setId_func_age(1);
-
-        if (agendamentoDAO.Create(age)) {
-            System.out.println("Teste bem sucedido, salvo com sucesso!");
-        } else {
-            fail("Teste falhou, não foi possível salvar");
-        }
-
-    }
+   
     /*
     @Test
     public void testUpdate() {
