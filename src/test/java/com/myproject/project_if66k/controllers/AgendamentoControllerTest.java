2770304e7d732fceb017fef68d6a876c7220d931
DanielNakano <dnakano@alunos.utfpr.edu.br>
2022-12-09 16:54:14 -0300

    fix issues #31

--- src/test/java/com/myproject/project_if66k/controllers/AgendamentoControllerTest.java
+++ src/test/java/com/myproject/project_if66k/controllers/AgendamentoControllerTest.java
@@ -30,6 +30,41 @@ public class AgendamentoControllerTest {
             fail("Teste falhou, não foi possível salvar");
         }
     }
+    
+    @Test
+    public void testeCreate(){
+        Agendamento age = new Agendamento();
+
+        age.setId_animal(1);
+        age.setServico_age("banho");
+        age.setData_age("10/10/2010 - 10:10");
+        age.setId_func_age(1);
+        
+        boolean resultado =  agendamentoDAO.Create(age);
+        
+        assertEquals(true, resultado);
+        
+        List<Agendamento> result = agendamentoDAO.readAll();
+        
+        Agendamento agen = new Agendamento();
+        
+        agen.setId_age(result.get(0).getId_age());
+        agen.setData_age(result.get(0).getData_age());
+        agen.setId_animal(result.get(0).getId_animal());
+        agen.setServico_age(result.get(0).getServico_age());
+        agen.setId_func_age(result.get(0).getId_func_age());
+        
+        String data = agen.getData_age();
+        String servico = agen.getServico_age();
+        int id_ani = agen.getId_animal();
+        int id_func = agen.getId_func_age();
+        
+        assertEquals("10/10/2010 - 10:10", data);
+        assertEquals("banho", servico);
+        assertEquals(1, id_ani);
+        assertEquals(1, id_func);
+        
+    }
 
     @Test
     public void testReadAll() {
@@ -38,6 +73,8 @@ public class AgendamentoControllerTest {
         List<Agendamento> result = agendamentoDAO.readAll();
         assertNotNull(result);
     }
+    
+    //bill clinton
  
     @Test
     public void testUpdate() {
@@ -60,5 +97,63 @@ public class AgendamentoControllerTest {
                 fail("Teste falhou, não foi possível salvar"); 
             }
         }        
+    }
+    
+    @Test
+    public void testeUpdate(){
+        Agendamento age = new Agendamento();
+        
+        age.setId_animal(4);
+        age.setData_age("10/10/2010 - 10:10");
+        age.setId_func_age(2);
+        age.setServico_age("tosa");
+        
+        agendamentoDAO.Create(age);
+        
+        List<Agendamento> result = agendamentoDAO.readAll();
+        
+        Agendamento agen = new Agendamento();
+        
+        agen.setId_age(result.get(0).getId_age());
+        agen.setData_age(result.get(0).getData_age());
+        agen.setId_animal(result.get(0).getId_animal());
+        agen.setServico_age(result.get(0).getServico_age());
+        agen.setId_func_age(result.get(0).getId_func_age());
+        
+        agen.setData_age("10/10/2022 - 10:20");
+        
+        boolean resultado = agendamentoDAO.Update(agen);
+        
+        assertEquals(true, resultado);
+   
+    }
+    
+    @Test
+    public void testDeleta(){
+        Agendamento age = new Agendamento();
+        
+        age.setId_animal(4);
+        age.setData_age("10/10/2010 - 10:10");
+        age.setId_func_age(2);
+        age.setServico_age("tosa");
+        
+        agendamentoDAO.Create(age);
+        
+        List<Agendamento> result = agendamentoDAO.readAll();
+        
+        Agendamento agen = new Agendamento();
+        
+        agen.setId_age(result.get(0).getId_age());
+        agen.setData_age(result.get(0).getData_age());
+        agen.setId_animal(result.get(0).getId_animal());
+        agen.setServico_age(result.get(0).getServico_age());
+        agen.setId_func_age(result.get(0).getId_func_age());
+        
+        
+        boolean resultado = agendamentoDAO.delete(agen);
+        
+        assertEquals(true, resultado);
+        
+        
     }
 }
